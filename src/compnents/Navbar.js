0663182a7a9fdd435d415503ba633fbe6dc5102d
Shubhvrat <shubhvratkulkarni@gmail.com>
2024-09-29 20:53:39 +0530

    //Added width-full css

--- src/compnents/Navbar.js
+++ src/compnents/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav className="flex justify-between items-center p-6 bg-gray-800 shadow-lg">
+        <nav className="flex justify-between items-center p-6 bg-gray-800 shadow-lg w-full">
             <div className="text-2xl font-bold">Shubhvrat</div>
             <ul className="flex space-x-6">
                 <li className="hover:text-indigo-400"><a href="#home">Home</a></li>
